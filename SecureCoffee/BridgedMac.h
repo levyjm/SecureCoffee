90afa11219a99e748cec6984a882f37ee996418b
levyjm <levyjohnm@gmail.com>
2017-01-09 20:19:04 -0500

    Built template

---
+++ SecureCoffee/BridgedMac.h
@@ -0,0 +1,13 @@
+//
+//  BridgedMac.h
+//  SecureCoffee
+//
+//  Created by John on 1/9/17.
+//  Copyright © 2017 john. All rights reserved.
+//
+
+#ifndef BridgedMac_h
+#define BridgedMac_h
+
+
+#endif /* BridgedMac_h */
---
+++ SecureCoffee/BridgedMac.m
@@ -0,0 +1,9 @@
+//
+//  BridgedMac.m
+//  SecureCoffee
+//
+//  Created by John on 1/9/17.
+//  Copyright © 2017 john. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
